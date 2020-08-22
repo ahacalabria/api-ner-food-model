4d9bd114bff592f75393b4712c7b98f0f1757058
Afonso Henrique Anastácio Calábria <ahacalabria@gmail.com>
2020-08-22 16:57:55 -0300

    adicionando model + /extract working

--- model-api-v1.py
+++ model-api-v1.py
@@ -1,14 +1,34 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, send_from_directory
+import spacy
 
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+output_dir = "./foods-model"
+nlp = spacy.load(output_dir)
 
 @app.route('/')
-def nao_entre_em_panico():
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+def index():
+    # if request.headers.get('Authorization') == '42':
+    #     return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
     return jsonify({"message": "API MODEL FOOD - version 0.1"})
+
+@app.route('/extract', methods=['GET'])
+def extract():
+    menu = request.args.get('menu')
+    
+    print(menu)
+    doc = nlp(menu.lower())
+    items = []
+    for ent in doc.ents:
+        print(ent.label_, ent.text)
+        if ent.label_ == "FOOD":
+            items.append(ent.text)
+    return jsonify({"menu": items})
+
+@app.route('/log')
+def log():
+    return send_from_directory('serving_static/templates', 'log.html')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
