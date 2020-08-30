16a84d3327e873a01a0a24e85f5b9f4989d5db5c
Afonso Henrique Anastácio Calábria <ahacalabria@gmail.com>
2020-08-30 18:01:31 -0300

    trocando para uwsgi

--- app.py
+++ app.py
@@ -30,6 +30,6 @@ def log():
     return send_from_directory('serving_static/templates', 'log.html')
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
